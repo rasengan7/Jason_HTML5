f0d981644e107584152977e7dff3ce9024ac229f
rasengan7 <vishwa.vijay143@gmail.com>
2013-05-13 21:38:22 -0700

    Starting setup

---
+++ MyScript.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+//Home Function
+  $('#btnHome').click(function(){
+   alert('Home');
+  });
+  
+ //Create Function
+ $('#btnCreate').click(function(){
+   alert('Create');
+  });
+  
+  // Open Function
+  $('#btnOpen').click(function(){
+   alert('Open');
+  });
+  
+  // Watch Function
+  $('#btnWatch').click(function(){
+   alert('Watch');
+  });
+  
+  //Update
+  $('#btnUpdate').click(function(){
+   alert('Update');
+  });
+  
+});
